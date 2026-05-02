69860398cb4213ad48d9d5d92c2dbe377629ea47
Shlok342 <shloknayak12@gmail.com>
2026-05-03 00:27:57 +0530

    Fix import path for API in script.js

--- backend/static/script.js
+++ backend/static/script.js
@@ -1,5 +1,5 @@
 const container = document.querySelector(".books-container");
-import { API } from "./services/api.js";
+import { API } from "../services/api.js";
 let books = [];
 let activeBookId = null;
 let lastKnownGlobalStreak = 0;
