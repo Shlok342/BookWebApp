a11bf674f326314150dc6b7c55e72f2a8cf225fe
Shlok342 <shloknayak12@gmail.com>
2026-05-24 21:49:33 +0530

    "Resolved undefined errors in getBooks.js"

--- backend/static/modal_helper/getBooks.js
+++ backend/static/modal_helper/getBooks.js
@@ -1,5 +1,7 @@
 import { API } from "../api_service/api.js";
 import { applyFilters } from "../filters.js";
+import { store } from "../store.js";
+export const container = document.querySelector(".books-container");
 export async function getBooks() {
     try {
       store.books = await API.getBooks();
