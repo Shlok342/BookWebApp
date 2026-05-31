0cecf1437f2fc7494c30d4dfcb0c91b8a7413d5a
Shlok342 <shloknayak12@gmail.com>
2026-05-31 11:57:21 +0530

    "Fixed imports in init_theme.js"

--- backend/static/integration_handler/init.js
+++ backend/static/integration_handler/init.js
@@ -1,10 +1,10 @@
-import { getStats } from "./modal_helper/statsModal.js";
-import { getChallenges } from "./modal_helper/challengeModal.js";
-import { getBooks } from "./modal_helper/getBooks.js";
-import {initQuotesModal} from "./modal_helper/quotesModal.js";
-import {initNotesModal} from "./modal_helper/notesModal.js";
-import {initQuoteOfDayModal} from "./shows_message/quoteOfTheDayModal.js";
-import { initThemeToggle,} from "./theme.js";
+import { getStats } from "../modal_helper/statsModal.js";
+import { getChallenges } from "../modal_helper/challengeModal.js";
+import { getBooks } from "../modal_helper/getBooks.js";
+import {initQuotesModal} from "../modal_helper/quotesModal.js";
+import {initNotesModal} from "../modal_helper/notesModal.js";
+import {initQuoteOfDayModal} from "../shows_message/quoteOfTheDayModal.js";
+import { initThemeToggle,} from "../theme.js";
 import { scheduleMidnightCheck, getGlobalStreak } from "./streak_helper/streak_helper.js";
 export function initMain(){
     initThemeToggle();
