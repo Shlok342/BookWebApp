2472b585f8324e0b554159c8649888e849d3efe6
Shlok342 <shloknayak12@gmail.com>
2026-06-02 20:17:35 +0530

    "Removed quote of the day from frontend but kept the backend calls"

--- backend/static/integration_handler/init.js
+++ backend/static/integration_handler/init.js
@@ -3,13 +3,11 @@ import { getChallenges } from "../modal_helper/challengeModal.js";
 import { getBooks } from "../modal_helper/getBooks.js";
 import {initQuotesModal} from "../modal_helper/quotesModal.js";
 import {initNotesModal} from "../modal_helper/notesModal.js";
-import {initQuoteOfDayModal} from "../shows_message/quoteOfTheDayModal.js";
 import { initThemeToggle,} from "../theme.js";
 import { scheduleMidnightCheck, getGlobalStreak } from "../streak_helper/streak_helper.js";
-import { initHeatmap} from "../heatmap/heatmap.js";
 export function initMain(){
     initThemeToggle();
-    initQuoteOfDayModal();
+    
     initQuotesModal();
 // ─── NOTES MODAL ──────────────────────────────────────────────────────────────
     initNotesModal();
